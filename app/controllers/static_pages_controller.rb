5f13d8a3bed222911ac9aa7875e15e77f65e8087
shawniscool <xiangpan@dhcp-130-58-192-137.swarthmore.edu>
2015-04-30 23:03:41 -0400

    testing heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,16 +5,18 @@ class StaticPagesController < ApplicationController
 
   def call
   	system 'echo hi'
-  	result = `python print.py`
+    result = `python python/print.py`
   	puts "#{result}"
   	# returned is a string separated by comma
   	separate = result.split(",")
   	puts separate[0]
   	puts separate[1]
   	puts separate[2]
-    file = File.read('myData.json')
-    data_hash = JSON.parse(file)
-    puts data_hash
+    # file = File.read('myData.json')
+    # data_hash = JSON.parse(file)
+    # puts data_hash
+    clothes = `python python/clothesAdvisor.py`
+
     
   	redirect_to root_url
 
